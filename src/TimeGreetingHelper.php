6e39e34e0e236061f08c5845ecfe895948d09157
Jon Morby <jon@morby.org>
2025-02-23 22:49:29 +0000

    initial import

---
+++ src/TimeGreetingHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace jpmorby\TimeGreetingHelper;
+
+/**
+ * Summary of TimeGreetingHelper
+ * @author Jon Morby
+ * @copyright (c) 2025
+ */
+class TimeGreetingHelper
+{
+    public static function getGreeting()
+    {
+        $hour = date('H');
+
+        if ($hour < 12) {
+            return 'Good morning';
+        } elseif ($hour < 18) {
+            return 'Good afternoon';
+        } else {
+            return 'Good evening';
+        }
+    }
+}
